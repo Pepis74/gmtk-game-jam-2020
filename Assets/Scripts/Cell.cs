66f26251bb7c1ec24aec8c44a068c179415a7a4c
Pepis74 <68124006+Pepis74@users.noreply.github.com>
2020-07-11 04:15:55 +0200

    Added cells

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [HideInInspector]
+    public bool viable;
+    [HideInInspector]
+    public int cost;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        anim.SetBool("start", true);
+    }
+
+    void OnMouseExit()
+    {
+        anim.SetBool("start", false);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    Transform cellParent;
+    [SerializeField]
+    GameObject cell;
+    GameObject ins;
+    [SerializeField]
+    Vector2 oGCellPos;
+    Cell[] cells = new Cell[64];   
+    int crescendoA;
+    
+    void Start()
+    {
+        oGCellPos = new Vector2(oGCellPos.x + 0.09f, oGCellPos.y + 0.06f);
+
+        for (int i = 0; i < 64; i++)
+        {
+            if(crescendoA == 8)
+            {
+                crescendoA = 0;
+                oGCellPos = new Vector2(oGCellPos.x + 0.09f * 9, oGCellPos.y + 0.06f * 7);
+            }
+
+            ins = Instantiate(cell, Vector2.zero, cell.transform.rotation);
+            ins.transform.parent = cellParent;
+            oGCellPos.x -= 0.09f;
+            oGCellPos.y -= 0.06f;
+            ins.transform.localPosition = oGCellPos;
+            ins.transform.localScale = Vector3.one;
+            cells[i] = ins.GetComponentInChildren<Cell>();
+            crescendoA += 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
