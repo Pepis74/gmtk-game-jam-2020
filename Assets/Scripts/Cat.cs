0985452a4a1e8d1f331550f88c6c626eece9a390
Jorge Fabregat <jorgepil508@gmail.com>
2020-07-11 11:12:11 +0200

    Added cat stub and definitions class

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    // Personality structure
+    struct Personality
+    {
+        public int[] stamina;
+    }
+
+    // Personality
+    Personality pers = new Personality();
+    // Cell where the cat is located
+    public int position;
+    // Max movement range of the cat. Personality Stat v
+    public int stamina;
+    // 
+    public float sleep_chances;
+    public float locker_chances;
+    public int[] personality_mods;
+    public int[][] totd_mods;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GeneratePersonality()
+    {
+        // TODO
+        pers.stamina = new int[Definitions.NO_OF_TOTD_SLOTS];
+    }
+
+    void UpdateStatsTotd()
+    {
+        // TODO
+    }
+
+    int GetNextCell(int position, int stamina)
+    {
+        // TODO 
+        return 0;
+    }
+
+
+}
---
+++ Assets/Scripts/Definitions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Definitions : MonoBehaviour
+{
+    // Define constant values here
+    public const int NO_OF_TOTD_SLOTS = 4;
+}
